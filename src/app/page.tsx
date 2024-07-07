fa2aebb6b8aa98711c9f34589cbac79498f7f997
youngsikkk <126939845+youngsikkk@users.noreply.github.com>
2024-07-07 15:25:52 +0900

    feat : ealry click prevention

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Home() {
   const [isWaiting, setIsWaiting] = useState(false);
@@ -10,17 +10,28 @@ export default function Home() {
   const [backColor, setBackColor] = useState("red");
   const [scores, setScores] = useState<number[]>([]);
   const [message, setMessage] = useState<string>("게임을 시작하려면 클릭해주세요");
+  const timerIdRef = useRef<any>(null);
 
   // 파란화면 -> 초록화면으로 랜덤하게 바뀌기
   const blockClick = () => {
     const waitTime = Math.floor(Math.random() * 10) + 1;
-    setTimeout(() => {
+    timerIdRef.current = setTimeout(() => {
       const ti = Date.now();
       setStartTime(ti);
       setBackColor("green");
       setMessage("클릭하세요!");
+      setIsWaiting(false);
     }, waitTime * 1000);
   }
+
+  const reStart = () => {
+    setIsWaiting(true);
+    setBackColor("blue");
+    setMessage("화면이 바뀔 때까지 기다려주세요.");
+    blockClick();
+  }
+
+  
 
   const gameStart = () => {
     if(isStart) return;
@@ -32,7 +43,19 @@ export default function Home() {
   }
    
   const timeCheck = () => {
-    if(isClick) return;
+    if(isWaiting) {
+      setStartTime(0);
+      setBackColor("gray");
+      setMessage("너무 빨리 클릭했습니다. 곧 대기 화면으로 전환됩니다.");
+      clearTimeout(timerIdRef.current);
+      timerIdRef.current = setTimeout(() => {
+        setBackColor("blue");
+        setMessage("화면이 바뀔 때까지 기다려주세요.");
+        setIsWaiting(true);
+        blockClick();
+      }, 2000);
+      return;
+    }
     setIsClick(true);
     const ti = Date.now();
     const sc = ti - startTime;
@@ -40,11 +63,17 @@ export default function Home() {
     setBackColor("blue");
     setStartTime(0);
     setMessage("화면이 바뀔 때까지 기다려주세요.");
+    setIsWaiting(true);
+    clearTimeout(timerIdRef.current);
     blockClick();
     setIsClick(false);
   }
 
-
+  useEffect(() => {
+    return () => {
+      clearTimeout(timerIdRef.current); // 컴포넌트 언마운트 시 타이머 정리
+    };
+  }, []);
 
   return (
     <div style={{display:"flex", flexDirection:"column", alignItems: "center"}}>
@@ -56,7 +85,7 @@ export default function Home() {
       </div>
       <div style = {{display: "flex", flexDirection:"column", alignItems: "center", marginTop: "20px"}}>
         {scores.map((score, idx) => {
-          return <span>{idx + 1}. {score}ms</span>
+          return <span>{idx + 1}번째 시도 {score}ms</span>
         })}
       </div>
     </div>
